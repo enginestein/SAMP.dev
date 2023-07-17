23ae3bb0858032abbf053844b6ef21218d1d6732
Arya Praneil Pritesh <aryapraneil@gmail.com>
2023-07-17 18:41:42 +0530

    Add files via upload

---
+++ SAMP.dev/SAMP.dev/dllmain.cpp
@@ -0,0 +1,20 @@
+#include "dllmain.hpp"
+#include <Utils/Windows.hpp>
+#include <Game/Game.hpp>
+
+HANDLE g_hDllHandle = 0;
+
+BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID)
+{
+	g_hDllHandle = hInstance;
+
+	DisableThreadLibraryCalls((HMODULE) hInstance);
+
+	if (dwReason != DLL_PROCESS_ATTACH)
+		return FALSE;
+
+	if (GetModuleHandle("samp.dll"))
+		return CreateThread(0, 0, (LPTHREAD_START_ROUTINE) initGame, 0, 0, 0) > 0;
+
+	return TRUE;
+}
---
+++ SAMP.dev/SAMP.dev/dllmain.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include <Utils/Windows.hpp>
+
+extern HANDLE g_hDllHandle;
